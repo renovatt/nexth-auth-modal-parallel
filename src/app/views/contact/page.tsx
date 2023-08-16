c609756e1353e254aa364ce7f59b72cc4d6baa82
will <wlymes@gmail.com>
2023-08-16 11:36:49 -0300

    feat: routes added

---
+++ src/app/views/contact/page.tsx
@@ -0,0 +1,3 @@
+export default function Contact() {
+  return <h2 className="text-white">Contato</h2>
+}
---
+++ src/app/views/shop/page.tsx
@@ -0,0 +1,3 @@
+export default function Shop() {
+  return <h2 className="text-white">Shop</h2>
+}
