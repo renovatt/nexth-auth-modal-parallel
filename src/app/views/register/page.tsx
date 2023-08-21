27000d3561c1b33a5ebd6bdaa709b5fd55089dd5
will <wlymes@gmail.com>
2023-08-21 11:09:07 -0300

    feat: register router

---
+++ src/app/views/register/page.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { useSession } from 'next-auth/react'
+import { RegisterModal } from '@/app/components/Register'
+
+export default function Register() {
+  console.log('original route register')
+  const { data: session } = useSession()
+
+  return (
+    <>
+      {!session && (
+        <RegisterModal.Root>
+          <RegisterModal.Content>
+            <RegisterModal.CloseIcon />
+            <RegisterModal.ThumbContainer>
+              <RegisterModal.Thumbnail />
+            </RegisterModal.ThumbContainer>
+            <RegisterModal.FormContainer>
+              <RegisterModal.Form />
+              <RegisterModal.SignIn />
+            </RegisterModal.FormContainer>
+          </RegisterModal.Content>
+        </RegisterModal.Root>
+      )}
+    </>
+  )
+}
