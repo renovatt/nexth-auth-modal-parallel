6c580adfa773c2cb967a5121ed635a66fce4e199
will <wlymes@gmail.com>
2023-08-22 10:44:20 -0300

    feat: login route api created

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,43 @@
+// import * as bcrypt from 'bcrypt'
+import prisma from '@/lib'
+// import { signJwtAcessToken } from '@/lib/jwt'
+import { LoginSchemaProps } from '@/@types'
+import { BadRequesError, InternalError, NotFoundError } from '@/errors'
+
+export async function POST(request: Request) {
+  try {
+    const body: LoginSchemaProps = await request.json()
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: body.email,
+      },
+    })
+
+    if (!user) throw new NotFoundError('Usuário não encontrado.')
+
+    // const passwordMatch = await bcrypt.compare(body.password, user.password)
+    // if (!passwordMatch) throw new BadRequesError('Senha está incorreta.')
+
+    if (user) {
+      const { password, ...userWithouPass } = user
+      // const accessToken = signJwtAcessToken(userWithouPass)
+
+      // const result = { ...userWithouPass, accessToken }
+      const result = { ...userWithouPass }
+
+      return new Response(JSON.stringify({ result, message: 'Bem vindo, ' }))
+    }
+  } catch (error: any) {
+    if (!error.statusCode) error = new InternalError('Internal server error')
+
+    return new Response(
+      JSON.stringify({
+        error: error.message,
+      }),
+      {
+        status: error.statusCode,
+      },
+    )
+  }
+}
